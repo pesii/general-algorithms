27baefbe5a47f6e8922036d08b73dd3e365984c2
Pesi <pesii@example.com>
2017-05-28 20:42:52 -0700

    changed a few things

--- algorithms.c
+++ algorithms.c
@@ -1,21 +1,18 @@
 #include "algorithms.h"
 
-#define ASCII_32 32
-#define ASCII_47 47
-#define ASCII_58 58
-#define ASCII_64 64
-#define ASCII_91 91
-#define ASCII_96 96
-#define ASCII_123 123
-#define ASCII_127 127
-#define ASCII_ALPHABET_A 65
-#define ASCII_ALPHABET_Z 90
-#define ASCII_ALPHABET_a 97
-#define ASCII_ALPHABET_z 122
-#define ASCII_65_TO_97 32
+#define ASCII_65 65
+#define ASCII_90 90
+#define ASCII_97 97
+#define ASCII_122 122
+#define UPPER_TO_LOWER 32
 
 #define ANAGRAM 1
 #define NOT_ANAGRAM 0
+
+typedef struct occurence {
+	int count[26];
+} Occurence;
+
 
 /*
  * Returns the length of the input string
@@ -37,8 +34,9 @@ int strLength(char strFeed[]){
 void puncStrip(char stringFeed[]){
 	int i = 0, j = 0;
 	while(stringFeed[i] != '\0') {
-		if ((stringFeed[i] >= ASCII_ALPHABET_A) && (stringFeed[i] <= ASCII_ALPHABET_Z)) {
-			stringFeed[j] = stringFeed[i] + ASCII_65_TO_97; // explicit casting of an integer as a character... unnecessary though
+	
+		if ((stringFeed[i] >= ASCII_65) && (stringFeed[i] <= ASCII_90)) {
+			stringFeed[j] = stringFeed[i] + UPPER_TO_LOWER;
 			j++;
 		} else if ((stringFeed[i] >= ASCII_ALPHABET_a) && (stringFeed[i] <= ASCII_ALPHABET_z)) {
 			stringFeed[j] = stringFeed[i];
@@ -48,10 +46,6 @@ void puncStrip(char stringFeed[]){
 	}
 	stringFeed[j] = '\0'; // Recalibrating the end of string after changing its contents
 }
-
-typedef struct occurence {
-	int count[26];
-} Occurence;
 
 /*
  * Returns 0 if NOT an anagram,
